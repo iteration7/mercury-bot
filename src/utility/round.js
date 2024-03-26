ff431deb43979de1bc709b6af135988ce2007cf4
Seven <julianshearer1@gmail.com>
2024-03-26 23:10:25 +0000

    Fixed number rounding bug

--- src/utility/round.js
+++ src/utility/round.js
@@ -1,35 +1,42 @@
 export default (num) => {
-  const suffix = ["", "k", "m", "b", "t", "q", "s", "o"];
-  var char = "";
-  var slice = null;
+const suffix = ["", "k", "m", "b", "t", "q", "s", "o"];
+    var char = "";
+    var dec = false;
+    var slice = null;
 
-  for (var i in suffix) {
-    i = Number(i);
-    const max = 1000 ** (i + 1);
-    const min = 1000 ** i;
-    if (num >= min && num < max) {
-      //a tenth
-      if (num >= max / 10) {
-        slice = 3;
-      }
-      //a hundreth
-      else if (num >= max / 100) {
-        slice = 2;
-      }
-      //a thousandth
-      else {
-        slice = 1;
-      }
+    for (var i in suffix) {
+      i = Number(i);
+      const max = 1000 ** (i + 1);
+      const min = 1000 ** i;
+      if (num >= min && num < max) {
+        //a tenth
+        if (num >= max / 10) {
+          slice = 3;
+          
+        }
+        //a hundreth
+        else if (num >= max / 100) {
+          slice = 2;
+          
+        }
+        //a thousandth
+        else {
+          slice = 1;
+          dec=true;
+        }
 
-      char = suffix[i];
-      break;
+        char = suffix[i];
+        break;
+      }
     }
-  }
-
-  if (num < 1000) num = num;
-  else
-    num =
-      String(num).slice(0, slice) + "." + String(num).slice(slice, slice + 1);
-
-  return num + char;
+    var fixedNum;
+    
+    if (num < 1000) {
+      if(num > 0) fixedNum = num.toFixed(1);
+      else fixedNum=num;
+    }
+    else fixedNum = String(num).slice(0, slice);
+    if (dec && num>=1000) fixedNum += '.' + String(num).slice(slice, slice+1);
+    
+    return fixedNum + char;
 };
